792bfb9c2d7257b60166e14ea654a24b92f3035e
Jesus Jose Garcia Pardo <a271305@uach.mx>
2015-11-01 11:32:59 -0700

    Agregando Tags

---
+++ blog/app/models/Tag.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+
+@Entity
+public class Tag extends Model implements Comparable<Tag> {
+
+    public String name;
+
+    private Tag(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public int compareTo(Tag otherTag) {
+        return name.compareTo(otherTag.name);
+    }
+
+    public static Tag findOrCreateByName(String name) {
+        Tag tag = Tag.find("byName", name).first();
+        if (tag == null) {
+            tag = new Tag(name);
+        }
+        return tag;
+    }
+
+    public static List<Map> getCloud() {
+        List<Map> result = Tag.find(
+                "select new map(t.name as tag, count(p.id) as pound) from Post p join p.tags as t group by t.name order by t.name"
+        ).fetch();
+        return result;
+    }
+}
